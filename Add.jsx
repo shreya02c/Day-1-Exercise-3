692fa3557cde47cb0c17e1bc9331935dc31b0298
shreya02c <shreyachaudhari996@gmail.com>
2025-03-18 15:06:42 +0530

    Add files via upload

---
+++ Add.jsx
@@ -0,0 +1,21 @@
+export function CourseGoal() {
+    return (
+      <li>
+        <h2>TITLE</h2>
+        <p>DESCRIPTION</p>
+      </li>
+    );
+  }
+  
+  function App() {
+    return (
+      <div id="app" data-testid="app">
+        <h1>Time to Practice</h1>
+        <p>One course, many goals! 🎯</p>
+        <ul>
+        </ul>
+      </div>
+    );
+  }
+  
+  export default App;
